e4c17b159af7aa01f81fb6acc3dd7ccf05d5a852
Jose Lee <jose.lee@conmio.com>
2013-09-20 09:58:56 +0300

    Default arg values added for listenClient.js

--- listenClient.js
+++ listenClient.js
@@ -1,7 +1,9 @@
 var io = require('socket.io-client');
 
 var users = parseInt(process.argv[2]);
+users = users ? users : 10;
 var rampUpTime = parseInt(process.argv[3]);
+rampUpTime = rampUpTime ? rampUpTime : 1000;
 var newUserTimeout = rampUpTime / users;
 var host = process.argv[4] ? process.argv[4]  : 'localhost';
 var port = process.argv[5] ? process.argv[5]  : '3000';
